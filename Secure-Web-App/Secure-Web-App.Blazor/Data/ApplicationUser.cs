148295dd5bed57ad913f543ef46f8f5d648001bf
Lukasjansen100 <lukas100@live.nl>
2024-09-23 11:18:32 +0200

    Re-created blazor template with identity installed

---
+++ Secure-Web-App/Secure-Web-App.Blazor/Data/ApplicationUser.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Secure_Web_App.Blazor.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+}
